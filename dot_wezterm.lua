8de5e35ead42edc6c724c9fd43a0bb90dc0a52bb
Jason Yang <jasyang@purestorage.com>
2026-09-03 12:31:29 -0600

    Use IosevkaTerm in WezTerm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,7 +4,7 @@ local wezterm = require 'wezterm'
 -- Hold the configuration builder object
 local config = wezterm.config_builder()
 config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font('Iosevka Nerd Font Mono')
+config.font = wezterm.font('IosevkaTerm Nerd Font Mono')
 config.font_size = 11
 config.window_background_opacity = 0.93
 config.window_decorations = "NONE"
